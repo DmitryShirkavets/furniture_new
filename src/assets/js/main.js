2e73381e80273ae933e9d9004b23ab488530e9e0
Zezina <daryazezina@gmail.com>
2023-04-13 09:25:45 +0300

    fix-global

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -207,7 +207,7 @@ function check(e) {
   if (!e.className.includes("active")) {
     e.className += " active";
   } else {
-    e.className = e.className.split(' active')[0];
+    e.className = e.className.split(" active")[0];
   }
   // e.currentTarget.classList.toggle('active')
 }
@@ -263,3 +263,190 @@ function check(e) {
 //     // };
 
 // }
+
+// function scrollFroze(e) {
+//   let element = document.querySelector('.product__measure-btn')
+//   let attr = element.getAttribute('href')
+//   let destination = attr.offset().top
+
+//   document.getElementsByTagName('body').animate({ scrollTop: destination }, 1400);
+// }
+// $(document).ready(function () {
+//   $(".product__measure-btn").click(function () {
+//     // 1
+//   });
+// });
+
+ymaps.ready(function () {
+  // Создание экземпляра карты и его привязка к созданному контейнеру.
+  var myMap = new ymaps.Map('map', {
+          center: [48.702269, 44.500922],
+          zoom: 16,
+          // behaviors: ['default']
+          controls: []
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      // Создание макета балуна на основе Twitter Bootstrap.
+      MyBalloonLayout = ymaps.templateLayoutFactory.createClass(
+          '<div class="popover top">' +
+              '</div>', {
+              /**
+               * Строит экземпляр макета на основе шаблона и добавляет его в родительский HTML-элемент.
+               * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/layout.templateBased.Base.xml#build
+               * @function
+               * @name build
+               */
+              build: function () {
+                  this.constructor.superclass.build.call(this);
+
+                  this._$element = $('.popover', this.getParentElement());
+
+                  this.applyElementOffset();
+
+                  this._$element.find('.close')
+                      .on('click', $.proxy(this.onCloseClick, this));
+              },
+
+              /**
+               * Удаляет содержимое макета из DOM.
+               * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/layout.templateBased.Base.xml#clear
+               * @function
+               * @name clear
+               */
+              clear: function () {
+                  this._$element.find('.close')
+                      .off('click');
+
+                  this.constructor.superclass.clear.call(this);
+              },
+
+              /**
+               * Метод будет вызван системой шаблонов АПИ при изменении размеров вложенного макета.
+               * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/IBalloonLayout.xml#event-userclose
+               * @function
+               * @name onSublayoutSizeChange
+               */
+              onSublayoutSizeChange: function () {
+                  MyBalloonLayout.superclass.onSublayoutSizeChange.apply(this, arguments);
+
+                  if(!this._isElement(this._$element)) {
+                      return;
+                  }
+
+                  this.applyElementOffset();
+
+                  this.events.fire('shapechange');
+              },
+
+              /**
+               * Сдвигаем балун, чтобы "хвостик" указывал на точку привязки.
+               * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/IBalloonLayout.xml#event-userclose
+               * @function
+               * @name applyElementOffset
+               */
+              applyElementOffset: function () {
+                  this._$element.css({
+                      left: -(this._$element[0].offsetWidth / 2),
+                      top: -(this._$element[0].offsetHeight + this._$element.find('.arrow')[0].offsetHeight)
+                  });
+              },
+
+              /**
+               * Используется для автопозиционирования (balloonAutoPan).
+               * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/ILayout.xml#getClientBounds
+               * @function
+               * @name getClientBounds
+               * @returns {Number[][]} Координаты левого верхнего и правого нижнего углов шаблона относительно точки привязки.
+               */
+              getShape: function () {
+                  if(!this._isElement(this._$element)) {
+                      return MyBalloonLayout.superclass.getShape.call(this);
+                  }
+
+                  var position = this._$element.position();
+
+                  return new ymaps.shape.Rectangle(new ymaps.geometry.pixel.Rectangle([
+                      [position.left, position.top], [
+                          position.left + this._$element[0].offsetWidth,
+                          position.top + this._$element[0].offsetHeight + this._$element.find('.arrow')[0].offsetHeight
+                      ]
+                  ]));
+              },
+
+              /**
+               * Проверяем наличие элемента (в ИЕ и Опере его еще может не быть).
+               * @function
+               * @private
+               * @name _isElement
+               * @param {jQuery} [element] Элемент.
+               * @returns {Boolean} Флаг наличия.
+               */
+              _isElement: function (element) {
+                  return element && element[0] && element.find('.arrow')[0];
+              }
+          }),
+
+      // Создание метки с пользовательским макетом балуна.
+      myPlacemark = window.myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          // balloonHeader: 'Заголовок балуна',
+          // balloonContent: 'Контент балуна'
+      }, {
+          balloonShadow: false,
+          // balloonLayout: MyBalloonLayout,
+          // balloonContentLayout: MyBalloonContentLayout,
+          balloonPanelMaxMapArea: 0
+          // Не скрываем иконку при открытом балуне.
+          // hideIconOnBalloonOpen: false,
+          // И дополнительно смещаем балун, для открытия над иконкой.
+          // balloonOffset: [3, -40]
+      });
+
+  myMap.geoObjects.add(myPlacemark);
+});
+
+
+
+
+
+
+
+if (document.getElementById("phone") !== undefined) {
+  let phone = document.getElementById("phone");
+
+  let maskOptions = {
+    mask: "+7(000)000-00-00",
+    lazy: false,
+  };
+  let mask = new IMask(phone, maskOptions);
+}
+
+if (document.getElementById("email") !== undefined) {
+  let element2 = document.getElementById("email");
+  let maskOptions2 = {
+    mask: function (value) {
+      if (/^[a-z0-9_\.-]+$/.test(value)) return true;
+      if (/^[a-z0-9_\.-]+@$/.test(value)) return true;
+      if (/^[a-z0-9_\.-]+@[a-z0-9-]+$/.test(value)) return true;
+      if (/^[a-z0-9_\.-]+@[a-z0-9-]+\.$/.test(value)) return true;
+      if (/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}$/.test(value)) return true;
+      if (/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}\.$/.test(value)) return true;
+      if (/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}\.[a-z]{1,4}$/.test(value))
+        return true;
+      return false;
+    },
+    lazy: false,
+  };
+  let mask2 = new IMask(element2, maskOptions2);
+}
+
+if (document.querySelector(".select-choices") !== undefined) {
+  const choiceElement = document.querySelector(".select-choices");
+
+  const choices = new Choices(choiceElement, {
+    searchEnabled: false,
+    itemSelectText: "",
+  });
+}
+
